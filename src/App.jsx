87e76d7dfaf74882ae821586ddfccf2b43451a4f
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-03-31 16:56:08 +0600

    Pointer add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Banner from './Components/Banner'
 import Navbar from './Components/Navbar'
+import Pointer from './Components/Pointer'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navbar/>
       <Banner/>
+      <Pointer/>
     </>
   )
 }
---
+++ src/Components/Pointer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Pointer = () => {
+    return (
+        <div className="bg-[#6f07f7] my-5">
+            <div className='flex justify-around md:max-w-10/12 mx-auto p-8'>
+                <div className='space-y-3'>
+                    <p className='text-4xl font-bold text-white'>50K+</p>
+                    <p className='text-gray-300'>Active Users</p>
+                </div>
+                <div className='space-y-3'>
+                    <p className='text-4xl font-bold text-white'>200+</p>
+                    <p className='text-gray-300'>Premium Tools</p>
+                </div>
+                <div className='space-y-3'>
+                    <p className='text-4xl font-bold text-white'>4.9</p>
+                    <p className='text-gray-300'>Rating</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Pointer;
